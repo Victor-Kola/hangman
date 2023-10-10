cf971ca7ff69b0f651cda2c8cfcdafc87053550d
Victor-Kola <victor.kolawole@hotmail.com>
2023-10-10 23:45:53 +0100

    Milestone 3 which includes our input and check defnitions

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,6 +1,27 @@
-while True:
-    guess = input("Guess a letter ")
-    if len(guess) == 1 and guess.isalpha():
-        break
+import random
+favourite_fruits = ["banana", "watermelon", "apple", "cherries", "black grapes"]
+print(favourite_fruits)
+
+word = random.choice(favourite_fruits)
+
+
+
+
+def check_guess(guessed_letter):
+    lowercase_guess = guessed_letter.lower()
+    if guessed_letter in word:
+        print(f"Good GUESS! {guessed_letter} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character")
+        print(f"Sorry, {guessed_letter} is not in the word. Try again")
+
+def ask_for_input():
+    while True:
+        guessed_letter = input("Guess a letter ")
+        if len(guessed_letter) == 1 and guessed_letter.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character")
+    check_guess(guessed_letter)
+
+ask_for_input()
+
